d5517b0e9c7de5275b1332c1959daa6ba92b7d74
leeagles-pi <pi@localhost>
2017-03-28 13:16:25 +0000

    more changes

--- Temperature/led.py
+++ Temperature/led.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(4, GPIO.OUT)
-print "LED ON"
-GPIO.setup(4, GPIO.HIGH)
-time.sleep(1)
-print "LED OFF"
-GPIO.setup(4, GPIO.LOW)
 
+print "the LED ON"
+GPIO.setup(4, True)
+time.sleep(1)
+print "the LED OFF"
+GPIO.setup(4, False)
+GPIO.cleanup()
--- Temperature/sensorGUI.py
+++ Temperature/sensorGUI.py
@@ -3,9 +3,40 @@ from tkinter import *
 # import random num generator functions
 from random import randint
 #sensor read
+
+
+def drawButton1():
+    # add a button called ButtonA
+    buttonA = Button(window, text="Read Sensor", command=sensorOn)
+    buttonA.pack()
+
+
+def drawButton2():
+    buttonB = Button(window, text="Sensor Off", command=sensorOff)
+    buttonB.pack()
+
+def drawButton2():
+    buttonB = Button(window, text="Sensor On", command=sensorOn)
+    buttonB.pack()
+
+def drawWindow(status):
+    # start a window
+    window = Tk()
+    # add a text area called text
+    text = Text(window, width=10, height=1, fg="blue", bg="white")
+    # use the pack() function to add components to window
+    text.pack()
+
+    drawButton1()
+    if (status == 1):
+        drawButton2()
+    else:
+        drawButton3()
+
 def sensorOn():
          text.delete(0.0, END)
          text.insert(END, str(randint(0, 10)))
+         drawWindow(1)
          #do GPIO commands
          #turn LED on
          #read sensor
@@ -13,22 +44,10 @@ def sensorOn():
 def sensorOff():
          text.delete(0.0, END)
          text.insert(END, "Sensor Off")
+         drawWindow(0)
          #do GPIO commands
          #turn LED off
-         
-# start a window
-window = Tk()
 
-# add a text area called text
-text = Text(window, width=10, height=1, fg="blue", bg="white")
 
-# add a button called ButtonA
-buttonA = Button(window, text="Read Sensor", command=sensorOn)
-buttonB = Button(window, text="Sensor Off", command=sensorOff)
 
-# use the pack() function to add components to window
-
-text.pack()
-buttonA.pack()
-buttonB.pack()
-
+drawWindow(1)    
